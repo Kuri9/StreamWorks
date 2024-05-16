ac427fa266826a7683e74ac5ee9740f506f3ac43
Kuri9 <chris.main@gmail.com>
2024-05-16 11:47:28 +0900

    Setup for the Twitch API/Eventsub connections.

---
+++ StreamWorks/StreamWorks/Services/BackgroundServiceManager.cs
@@ -0,0 +1,24 @@
+﻿
+namespace StreamWorks.Services;
+public class BackgroundServiceManager : IHostedService
+{
+    ILogger<BackgroundServiceManager> logger;
+
+    public BackgroundServiceManager(ILogger<BackgroundServiceManager> logger)
+    {
+        this.logger = logger;
+        
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("BackgroundServiceManager has started.");
+        Console.WriteLine("BackgroundServiceManager has started.");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("BackgroundServiceManager has ended.");
+        Console.WriteLine("BackgroundServiceManager has ended.");
+    }
+}
